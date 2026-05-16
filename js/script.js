51b3b6dd3e09b353097a7e179d0fb2efbed894c6
morrowind1 <bebebarley@gmail.com>
2026-05-16 14:47:24 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-eferggrgrgrgg
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+});
+
+const cards = document.querySelectorAll('.project-card, .skill-card');
+
+cards.forEach((card) => {
+    observer.observe(card);
+});
